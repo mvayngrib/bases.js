f4cfffeb7aeee805e1e49b097d288cde551edfda
Mark Vayngrib <mark.vayngrib@lablz.com>
2015-01-02 19:34:50 -0500

    fix throw error

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ var to = process.argv[3];
 var text = process.argv[4];
 
 if (!bases.KNOWN_ALPHABETS[from] || !bases.KNOWN_ALPHABETS[to]) 
-  throw new Error('Valid bases are:', Object.keys(bases.KNOWN_ALPHABETS).join(','));
+  throw new Error('Valid bases are: ' + Object.keys(bases.KNOWN_ALPHABETS).join(','));
 
 console.log(bases.toBase(bases.fromBase(text, from), to));
